a6d1beefe8711a1f5466dae368bde58cab3929e3
lear23 <130910243+lear23@users.noreply.github.com>
2024-04-20 11:31:05 +0200

    fixa kompnenterna i features

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import Footer from './components/footer/Footer.jsx';
 import Header from './components/header/Header.jsx';
+import Features from './components/home/features/Features.jsx';
+import Showcase from './components/home/showcase/Showcase.jsx';
 
 
 function App() {
    return (
     <div className='wrapper'>
        <Header/>
-
+        <main>
+         <Showcase/>
+         <Features/>
+        </main>
        <Footer/>
     </div>
   )
